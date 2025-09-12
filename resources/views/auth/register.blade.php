13d1b4c78b5e3c8900a2e834729baaa9e43d54d1
ojulio-dev <juliocontact15@gmail.com>
2025-09-12 10:36:13 -0300

    Registrando um usuário

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,19 +1,34 @@
 <div>
-    <h1>Login</h1>
+    <h1>Register</h1>
 
     @if ($message = session()->get('message'))
         <div>{{ $message }}</div>
         <br>
     @endif
 
-    <form action="{{ route('login') }}" method="post">
+    <form action="{{ route('register') }}" method="post">
         @csrf
+
+        <div>
+            <input name="name" placeholder="Name" value="{{ old('name') }}" />
+            @error('name')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br>
 
         <div>
             <input name="email" placeholder="Email" value="{{ old('email') }}" />
             @error('email')
                 <span>{{ $message }}</span>
             @enderror
+        </div>
+
+        <br>
+
+        <div>
+            <input name="email_confirmation" placeholder="Email Confirmation" />
         </div>
 
         <br>
@@ -27,6 +42,6 @@
 
         <br>
 
-        <button>Logar</button>
+        <button>Registrar</button>
     </form>
 </div>
