80b5515a43ebe5939a691c07835d035f0c6d51ef
ojulio-dev <juliocontact15@gmail.com>
2025-09-16 17:43:36 -0300

    Middlewares Guest e Auth

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-Route::post('/register', [RegisterController::class, 'register']);
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+});
+
+Route::get('/logout', LogoutController::class)->middleware('auth')->name('logout');
 
 Route::get('/dashboard', fn() => 'dashboard :: ' . auth()->id())->middleware('auth')->name('dashboard');
