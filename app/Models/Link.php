2c2e83400a2e0e5cbd8171053d2e99d165673a20
ojulio-dev <juliocontact15@gmail.com>
2025-09-18 11:26:49 -0300

    Refatorando ordenação para dentro do model

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -9,4 +9,36 @@ class Link extends Model
 {
     /** @use HasFactory<\Database\Factories\LinkFactory> */
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Function responsible for reorder the link
+     *
+     * @param int $to +1 for moving down and -1 for moving up
+     * @return void
+     */
+    private function move($to)
+    {
+        $order = $this->sort;
+        $newOrder = $order + $to;
+
+        $swapWith = $this->user->links()->where('sort', '=', $newOrder)->first();
+
+        $this->fill(['sort' => $newOrder])->save();
+        $swapWith->fill(['sort' => $order])->save();
+    }
+
+    public function moveUp()
+    {
+        $this->move(-1);
+    }
+
+    public function moveDown()
+    {
+        $this->move(+1);
+    }
 }
