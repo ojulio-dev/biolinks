babdc7d622bbbc69844de87ebac5a64731383332
ojulio-dev <juliocontact15@gmail.com>
2025-09-25 09:38:59 -0300

    Blade Components

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,40 +11,4 @@
             </x-slot:actions>
         </x-card>
     </x-container>
-
-    <div class="mx-auto max-w-screen-md flex items-center justify-center py-20">
-
-        <div class="card bg-base-100 w-96 shadow-xl">
-            <div class="card-body">
-                <h1 class="card-title">Login</h1>
-
-                <form action="{{ route('login') }}" method="post" id="login-form">
-                    @csrf
-
-                    <div>
-                        <input class="input" name="email" placeholder="Email" value="{{ old('email') }}" />
-                        @error('email')
-                            <div class="text-sm text-error">{{ $message }}</div>
-                        @enderror
-                    </div>
-
-                    <br>
-
-                    <div>
-                        <input class="input" type="password" name="password" placeholder="Senha" />
-                        @error('password')
-                            <div class="text-sm text-error">{{ $message }}</div>
-                        @enderror
-                    </div>
-
-                    <br>
-
-                    <div class="card-actions">
-                        <button class="btn btn-primary" type="submit" form="login-form">Logar</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-    </div>
 </x-layout.app>
